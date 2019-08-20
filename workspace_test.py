4733ee1cce3458c41198488d9aba031c639e2e2d
Marianie-Simeon <msimeon@broadinstitue.org>
2019-08-20 15:26:56 -0400

    Create workspace_test.py

---
+++ workspace_test.py
@@ -0,0 +1,44 @@
+from firecloud import api
+from datetime import datetime
+import pprint
+import json
+import time
+
+clone_project = "featured-workspace-testing"
+clone_name = "Germline-SNPs-Indels-GATK4-hg38" +'_' + datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+res = api.clone_workspace("help-gatk",
+                          "Germline-SNPs-Indels-GATK4-hg38",
+                          clone_project,
+                          clone_name,
+                        )
+
+if res.status_code != 201:
+    print("Not Cloning")
+    print(res.text)
+    exit(1)
+
+res = api.list_workspace_configs(clone_project, clone_name, allRepos = True)
+if res.status_code != 200:
+    print(res.text)
+    exit(1)
+
+res = res.json()
+
+for item in res:
+    entityType = item["rootEntityType"]
+    namespace = item["namespace"]
+    name = item["name"]
+    entities = api.get_entities(clone_project, clone_name, entityType)
+    entityName = entities.json()[0]["name"]
+    ret = api.create_submission(clone_project, clone_name, namespace, name, entityName, entityType)
+    if ret.status_code != 201:
+        print(ret.text)
+        exit(1)
+
+
+while True:
+    res = api.list_submissions(clone_project, clone_name)
+    pprint.pprint(res.json())
+    time.sleep(30) 
+
+
