1a10672ca039eee9cac4d1d30aa2f6a33d25af74
Marianie-Simeon <msimeon@broadinstitue.org>
2019-08-28 11:04:30 -0400

    Updated push
    Updated push

---
+++ Test.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from firecloud import api
+namespace = "fccredits-curium-coral-4194"
+workspace = "Germline-SNPs-Indels-GATK4-b37-EX06test"
+res = api.list_submissions(namespace, workspace)
+res = res.json()
+for item in res:
+    count = 0
+    Failed = False
+    FailedMess = []
+    Link = []
+    for i in api.get_submission(namespace, workspace, item["submissionId"]).json()["workflows"]:
+        count +=1
+        if "workflowId" in i: 
+            Link.append(str(count) + ". " + "https://job-manager.dsde-prod.broadinstitute.org/jobs/"+ str(i["workflowId"]))
+            resworkspace = api.get_workflow_metadata(namespace, workspace, item["submissionId"], i["workflowId"]).json()
+            if resworkspace["status"] == "Failed":
+                for failed in resworkspace["failures"]:
+                    for message in failed["causedBy"]:
+                        if str(message["message"]) not in FailedMess:
+                            FailedMess.append(str(count) + ". " + str(message["message"]))
+                        Failed = True
+        else:
+            if i["status"] == "Failed":
+                print(item["submissionId"])
+                if "No Workflow Id" not in FailedMess:
+                    FailedMess.append("No Workflow Id")
+                Failed = True  
+    
+    print("End Results:")
+    if Failed:
+        print(str(item["methodConfigurationName"]) + " has Failed. The error message is: \n \n -"  + "\n \n -".join(FailedMess))
+        print("\n \n List of Links: - ")
+        print "\n-".join(Link)
+    else:
+        print(str(item["methodConfigurationName"]) + " has ran Successfully.")
+        print("\n \n List of Links: - ")
+        print "\n-".join(Link)
+    print("\n \n \n ")
+
+
+
+
+K =  datetime.today().strftime('%H:%M-%m/%d/%Y') + "<br>"
+
+f = open('hello.html','w')
+
+message = """<html>
+<head></head>
+<body><p><center>Terra's Feature Workspace Report</center> <br>
+<br>
+
+Name for Feature Workspace:
+<br><br> The Workflows Tested:
+<br><br> The Notebooks Tested:
+
+<br><br> {Ka} Cloning Workspace to:
+<br><br> {Ka} Running:
+<br><br> {Ka} ________ are all completed.
+<br> Everything ran successfully!</p></body>
+</html>"""
+
+message = message.format(Ka = K)
+f.write(message)
+f.close()
--- workspace_test.py
+++ workspace_test.py
@@ -1,36 +1,50 @@
+# Workspace_test.py is design to clone and run feature workspaces,
+# Afterwards, the program will generate a HTML report of failures and successes,
+# with linkes to their Workflow report on Terra.bio
+
 from firecloud import api
 from datetime import datetime
 import pprint
 import json
 import time
 
+# The names of where the the project and workspace, The feature workspace is being clone to this:
 clone_project = "featured-workspace-testing"
 clone_name = "Sequence-Format-Conversion" +'_' + datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+Orginal_name = "Sequence-Format-Conversion"
+Error_message = ""
+
+# Cloning the feature workspace
 res = api.clone_workspace("help-gatk",
                           "Sequence-Format-Conversion",
                           clone_project,
                           clone_name,
                         )
 
+#Catches if the feature workspace didn't clone
 if res.status_code != 201:
+    Error_message = "Not Cloning"
     print("Not Cloning")
     print(res.text)
     exit(1)
 
+#Catches if the cloned feature workspace had an error with loading
 res = api.list_workspace_configs(clone_project, clone_name, allRepos = True)
 if res.status_code != 200:
+    Error_message = res.text
     print(res.text)
     exit(1)
 
+#if cloning was successful, the program runs through the workspace and create a submission for each workflow
 res = res.json()
-methodNames = []
+workflow_names = []
 for item in res:
     entityType = None
     if "rootEntityType" in item:
         entityType = item["rootEntityType"]
     namespace = item["namespace"]
     name = item["name"]
-    methodNames.append(item["name"])
+    workflow_names.append(name)
     entities = api.get_entities(clone_project, clone_name, entityType)
     entityName = None
     if len(entities.json()) != 0:
@@ -42,6 +56,8 @@ for item in res:
 
 breakOut = False
 count = 0
+finish_workflows = []
+finish_workflows_details = []
 while not breakOut:
     res = api.list_submissions(clone_project, clone_name)
     res = res.json()
@@ -49,11 +65,37 @@ while not breakOut:
     for item in res:
         if item["status"] in terminal_states:
             count += 1
+            if item["methodConfigurationName"] not in finish_workflows:
+                details = str(item["methodConfigurationName"]) + " is finish at "+ datetime.today().strftime('%H:%M-%m/%d/%Y')
+                finish_workflows.append(item["methodConfigurationName"])
+                finish_workflows_details.append(details)
         if count == len(res):
             breakOut = True
     count = 0
     time.sleep(100)
-print ', '.join(methodNames) + " all ran successfully!"
+methodNamesstr = ', '.join(workflow_names)
+detailstr = "<br>".join(finish_workflows_details)
+print ', '.join(workflow_names) + " all ran successfully!"
 
+f = open('TerraFeatureWorkspaceReport.html','w')
 
+message = """<html>
+<head></head>
+<body><p><center>Terra's Feature Workspace Report</center> <br>
+<br>
+
+Name for Feature Workspace: {Feature_Workspace}
+<br><br> The Workflows Tested: {Workflows}
+<br><br> The Notebooks Tested: 0
+<br><br> Testing Project Name: {Project}
+<br><br> Cloning Workspace to: {Clone_Workspace}
+<br><br> Running: {Workflows}
+<br><br> Details:
+<br>{Details}
+<br><br>Everything ran successfully!</p></body>
+</html>"""
+
+message = message.format(Feature_Workspace = Orginal_name, Workflows = methodNamesstr, Project = clone_project, Clone_Workspace = clone_name, Details = detailstr)
+f.write(message)
+f.close()
 
