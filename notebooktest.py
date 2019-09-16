b9f2660a5a9a2bac68b7d86cd1b2578b8b43ff0d
Marianie-Simeon <msimeon@broadinstitue.org>
2019-09-16 13:54:36 -0400

    Update notebooktest.py

--- notebooktest.py
+++ notebooktest.py
@@ -2,8 +2,11 @@ from firecloud import api as fapi
 import json
 import subprocess
 
-#"featured-workspace-testing", "Terra_Quickstart_Workspace_MORGAN_manualclone"
-r = fapi.get_workspace("fc-product-demo", "Terra_Quickstart_Workspace")
+#"featured-workspace-testing", "Terra_Quickstart_Workspace_MORGAN_manualclone" - 4 Notebooks
+#fc-product-demo", "Terra_Quickstart_Workspace" - 4 Notebooks
+#"gmqltobroad" , "gmql" - 0 Notbooks
+r = fapi.get_workspace("featured-workspace-testing", "Terra_Quickstart_Workspace_MORGAN_manualclone")
+
 fapi._check_response_code(r, 200)
 workspace = r.json()
 bucket = workspace['workspace']['bucketName']
@@ -16,10 +19,17 @@ bucket_files = subprocess.check_output(gsutil_args, stderr=subprocess.PIPE)
 # Check output produces a string in Py2, Bytes in Py3, so decode if necessary
 if type(bucket_files) == bytes:
     bucket_files = bucket_files.decode()
-print(bucket_files)
-print(" ")
 print("Testing")
-gsutil_args = ['gsutil', 'cp', 'gs://' + bucket + '/notebooks/**', "gs://fc-324abe6c-3229-4e18-abcf-32c126de6bfd/notebooks"] 
-bucket_files = subprocess.check_output(gsutil_args, stderr=subprocess.PIPE)
-
+#TODO: Add a use case that if the bucket doesn't have notebooks
+if "/notebooks" in bucket_files:
+    print("Notebooks:")
+    gsutil_args = ['gsutil', 'ls', 'gs://' + bucket + '/notebooks/**']
+    #To copy over durning the clone
+    #gsutil_args = ['gsutil', 'cp', 'gs://' + bucket + '/notebooks/**', "gs://fc-324abe6c-3229-4e18-abcf-32c126de6bfd/notebooks"] 
+    bucket_files = subprocess.check_output(gsutil_args, stderr=subprocess.PIPE)
+    if type(bucket_files) == bytes:
+        bucket_files = bucket_files.decode()
+    print(bucket_files)
+else:
+    print("Workspace has no notebooks")
 #print(fapi.list_entity_types("featured-workspace-testing", "Terra_Quickstart_Workspace_MORGAN_manualclone").json())
