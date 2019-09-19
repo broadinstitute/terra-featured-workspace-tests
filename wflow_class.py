7b5f2855c2f364649422ac1c39c7499973c358df
Morgan Taylor <marymorg@broadinstitute.org>
2019-09-19 14:58:52 -0400

    remove old wflow class code

--- wflow_class.py
+++ wflow_class.py
@@ -1,55 +1,6 @@
 import os
 from dataclasses import dataclass
 
-class wflow_old:
-    def __init__(self, workspace, project, wfid, subid, wfname, entity, status, message=None):
-        self.workspace = workspace
-        self.project = project
-        self.wfid = wfid            # workflow id
-        self.subid = subid          # submission id
-        self.wfname = wfname        # workflow name
-        self.entity = entity
-        self.status = status
-        self.message = message
-        if wfid:
-            self.link = 'https://job-manager.dsde-prod.broadinstitute.org/jobs/'+ str(wfid)
-        else:
-            self.link = 'https://app.terra.bio/#workspaces/' + project + '/' + workspace + '/job_history/' + subid
-            
-    
-    def get_HTML(self):
-
-        if self.status == 'Failed':
-            status_color = 'red'
-            error_message = '<br>Error message: <font color=' + status_color + '>' + str(self.message) + '</font>'
-        elif self.status == 'Aborted':
-            status_color = 'orange'
-            error_message = ''
-        else:
-            status_color = 'green'
-            error_message = ''
-
-
-        message_html = '''
-        Workflow Id: {wfid}
-        <br>Submission Id: {subid}
-        <br>Entity Name: {entity}
-        <br>Status: <font color={status_color}>{status}</font>
-        {error_message}
-        <br><a href={link} target='_blank'>Click here for more details</a>
-        <br><br>
-        '''
-        message_html = message_html.format(wfname = self.wfname, 
-                            wfid = self.wfid,
-                            subid = self.subid,
-                            entity = self.entity,
-                            status_color = status_color,
-                            status = self.status,
-                            error_message = error_message,
-                            link = self.link)
-        return message_html
-
-
 @dataclass
 class wflow:
     '''Class for keeping track of info for Terra workflows.'''
