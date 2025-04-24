ff9ca566eb4427424f3be6163c257a734b7bb4ee
samanthahv <68919432+samanthahv@users.noreply.github.com>
2025-04-24 15:13:42 -0400

    [FE-395] Exclude GATK SV joint calling workspace from testing (#9)
    * exclude gatk sv joint calling workspace from testing
    
    * add logging

--- get_fws.py
+++ get_fws.py
@@ -106,6 +106,10 @@ def format_fws(get_info=False, verbose=True):
             print(f'Skipping Azure workspace {ws_project}/{ws_name}')
             continue
 
+        if ws_project == 'broad-firecloud-dsde-methods' and ws_name == 'GATK-Structural-Variants-Joint-Calling':
+            print(f'Skipping {ws_project}/{ws_name} for cost savings (FE-359)')
+            continue
+
         if verbose:
             print(ws_name + '\t (billing project: ' + ws_project + ')')
 
